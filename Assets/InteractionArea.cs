018dc3d9d2e260a15091388454a0c1732853f8b5
vickywinkel <vickywinkel@gmail.com>
2024-06-13 00:14:23 -0300

    Conect script + add value

--- Assets/InteractionArea.cs
+++ Assets/InteractionArea.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using System.Drawing;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InteractionArea : MonoBehaviour
 {
+    int puntaje;
+    public Text txtpuntos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        txtpuntos.text = "0"; 
     }
 
     // Update is called once per frame
@@ -19,6 +22,17 @@ public class InteractionArea : MonoBehaviour
     }
     private void OnTriggerEnter (Collider other)
     {
+        //le sumo a puntaje el valor de cada cosa contra la que colisione
+        puntaje = puntaje + other.gameObject.GetComponent<PickupScript>().valor;
+        txtpuntos.text = puntaje.ToString();
+
+        //destruyo el gameObject si es un alimento 
+        if (other.gameObject.GetComponent<PickupScript>().esAlimento == true)
+        {
+            Destroy(other.gameObject);
+        }
+        
+        //escribo conta que colisiono
         Debug.Log("Hubo colision con " + other.name);
     }
 
